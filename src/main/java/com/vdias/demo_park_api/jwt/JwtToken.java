53f0db90fe1bb38369c335c3b840c1e8aca3ee80
Vinicius <vi_aleixodias@hotmail.com>
2025-02-18 20:58:59 -0300

    feat/ created spring security config

---
+++ src/main/java/com/vdias/demo_park_api/jwt/JwtToken.java
@@ -0,0 +1,16 @@
+package com.vdias.demo_park_api.jwt;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
+public class JwtToken {
+    private String token;
+
+    
+}
